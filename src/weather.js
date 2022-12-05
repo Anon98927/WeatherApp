92c676f4d19892e218415954cc93f205a86ebd24
Anon98927 <chelle8788@gmail.com>
2022-12-05 12:20:34 -0500

    Update weather.js

--- src/weather.js
+++ src/weather.js
@@ -49,7 +49,7 @@ function displayForecast(response) {
       <div class="col-2">
         <div class="weather-forecast-date">${day}</div>
         <img
-          src="https://openweathermap.org/img/wn/01d@2x.png"
+          src="http://openweathermap.org/img/wn/01d@2x.png"
           alt=""
           width="36"
         />
@@ -91,7 +91,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
