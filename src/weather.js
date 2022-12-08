1114fa383a8010b9952812972dca97d6ee7d981d
Anon98927 <chelle8788@gmail.com>
2022-12-08 08:09:04 -0500

    12hr format

--- src/weather.js
+++ src/weather.js
@@ -29,6 +29,17 @@ function formatDay(timestamp) {
   return days[day];
 }
 
+const formatAMPM = (date) => {
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let ampm = hours >= 12 ? "pm" : "am";
+  hours = hours % 12;
+  hours = hours ? hours : 12;
+  minutes = minutes.toString().padStart(2, "0");
+  let strTime = hours + "" + minutes + "" + ampm;
+  return strTime;
+};
+
 function displayForecast(response) {
   let forecast = response.data.daily;
 
